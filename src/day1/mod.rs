3a01803e719ed695cab29390c515aa4326e8270b
Michael Gerhäuser <michael.gerhaeuser@methodpark.de>
2019-12-01 06:20:51 +0100

    Solve Day 1 problem 1 & 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,14 +1,52 @@
+fn fuel_for_one(mass: i32) -> i32 {
+  mass / 3 - 2
+}
+
+fn fuel_iterative(mass: i32) -> i32 {
+  let mut fuel = fuel_for_one(mass);
+  let mut total_fuel = fuel;
+
+  while fuel > 0 {
+    fuel = fuel_for_one(fuel);
+    if fuel > 0 {
+      total_fuel += fuel;
+    }
+  }
+
+  total_fuel
+}
+
+fn fuel_for_delivery(f: &dyn Fn(i32) -> i32) -> i32 {
+  let input = include_str!("./data/input.txt");
+  let total_fuel: i32 = input
+    .split("\n")
+    .filter(|v| *v != "")
+    .map(|v| v.parse::<i32>().unwrap())
+    .map(|m| f(m))
+    .sum();
+
+  total_fuel
+}
+
 pub fn problem1() {
-  println!("TBD");
+  println!("Fuel sum: {}", fuel_for_delivery(&fuel_for_one));
 }
 
 pub fn problem2() {
-//  let input = include_str!("./data/input_1-1.txt");
-//  let numbers = input
-//    .split("\n")
-//    .filter(|v| *v != "")
-//    .map(|v| v.parse::<i32>().unwrap())
-//    .collect::<Vec<_>>();
+  println!("Fuel sum iterative: {}", fuel_for_delivery(&fuel_iterative));
+}
 
-  println!("TBD");
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn iterative_fuel_example_1() {
+    assert_eq!(fuel_iterative(1969), 966);
+  }
+
+  #[test]
+  fn iterative_fuel_example_2() {
+    assert_eq!(fuel_iterative(100756), 50346);
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ mod day1;
 mod utils;
 
 fn run() -> Result<(), utils::Error> {
-  day1::problem1();
+  day1::problem2();
+
+  if false {
+    day1::problem1();
+  }
   Ok(())
 }
 
