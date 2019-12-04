1b1735ce0cb350d6b16895fbd8c6509288c90f0e
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2019-12-04 06:18:51 +0100

    WIP: Day4

---
+++ src/day4/mod.rs
@@ -0,0 +1,85 @@
+fn is_valid(p: u32) -> (bool, bool) {
+  let mut has_double = false;
+  let mut last_digit = 0;
+  let mut group_sizes = vec![1];
+
+  for i in 0..6 {
+    let digit = p / 10u32.pow(5 - i) % 10;
+    if last_digit > digit {
+      return (false, false);
+    }
+    if last_digit == digit {
+      has_double = true;
+      if let Some(v) = group_sizes.last_mut() {
+        *v += 1;
+      }
+    } else {
+      group_sizes.push(1);
+    }
+    last_digit = digit;
+  }
+
+  let valid_for_2 = group_sizes.contains(&2);
+
+  (has_double, valid_for_2)
+}
+
+pub fn problem1() -> u32 {
+  let start = 147981;
+  let end = 691423 + 1;
+
+  let mut count = 0;
+
+  for i in start..end {
+    if is_valid(i).0 {
+      count += 1;
+    }
+  }
+
+  println!("Count: {}", count);
+
+  count
+}
+
+pub fn problem2() -> u32 {
+  let start = 147981;
+  let end = 691423 + 1;
+
+  let mut count = 0;
+
+  for i in start..end {
+    if is_valid(i).1 {
+      count += 1;
+    }
+  }
+
+  println!("Count: {}", count);
+
+  count
+
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  pub fn problem1_ex1() {
+    assert_eq!(is_valid(111111u32), (true, false));
+  }
+
+  #[test]
+  pub fn problem1_ex2() {
+    assert_eq!(is_valid(223450u32), (false, false));
+  }
+
+  #[test]
+  pub fn problem1_ex3() {
+    assert_eq!(is_valid(123789u32), (false, false));
+  }
+  #[test]
+  pub fn problem2_ex3() {
+    assert_eq!(is_valid(111122u32), (true, true));
+
+  }
+}
