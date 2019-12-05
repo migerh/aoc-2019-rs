b900cf87737b427f563565f5f365b0311a77d1f2
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2019-12-05 20:52:57 +0100

    Day 5, Problem 2

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -2,59 +2,76 @@ fn isa_interpreter(instructions: &mut Vec<i32>, input: i32) -> i32 {
   let mut ip = 0;
   let mut op = instructions[ip];
 
-  let jump_table: Vec<usize> = vec![1, 4, 4, 2, 2];
+  let jump_table: Vec<usize> = vec![1, 4, 4, 2, 2, 3, 3, 4, 4];
+  let param_table: Vec<u32> = vec![1, 2, 2, 0, 1, 2, 2, 2, 2];
   let mut outputs = vec![];
 
-  println!("{:?}", instructions);
-
   while op != 99 {
-    let param_mode1 = op / 100 % 10;
-    let param_mode2 = op / 1000 % 10;
-
-    let param1 = instructions[ip + 1] as usize;
-    let param2 = instructions[ip + 2] as usize;
-    let result_address = instructions[ip + 3] as usize;
-
-    // for (idx, i) in instructions.iter().enumerate() {
-    //   println!("{}: {}", idx, i);
-    // }
-    println!("ip: {}, op: {}, p1: {} ({}), p2: {} ({}), r: {}", ip, op, param1, param_mode1, param2, param_mode2, result_address);
-
     let main_op = op % 10;
+    let num_params = param_table[main_op as usize];
 
-    let operand1 = if param_mode1 == 0 {
-      instructions[param1]
-    } else {
-      param1 as i32
-    };
-
-    let operand2 = if param_mode2 == 0 && main_op <= 2 {
-      instructions[param2]
-    } else {
-      param2 as i32
-    };
-
-    println!("ip: {}, op: {}, o1: {}, o2: {}, r: {}", ip, op, operand1, operand2, result_address);
-
-    if main_op <= 2 {
-      instructions[result_address] = match main_op {
-        1 => operand1 + operand2,
-        2 => operand1 * operand2,
-        _ => panic!("At the math disco!")
-      }
-    } else {
-      instructions[param1] = match main_op {
-        3 => input,
-        4 => {
-          println!("Output! {}", operand1);
-          outputs.push(operand1);
-          instructions[param1]
-        },
-      v => panic!("at the disco: {}", v)
+    let mut operands = vec![];
+    for i in 0..num_params {
+      let mode = op / 10i32.pow(i + 2) % 10;
+      let param = instructions[ip + i as usize + 1];
+      let operand = if mode == 0 {
+        instructions[param as usize]
+      } else {
+        param
       };
+      operands.push(operand);
     }
+    let result_index = instructions[ip + num_params as usize + 1] as usize;
 
-    ip += jump_table[main_op as usize];
+    println!("ip: {}, op: {}, operands: {:?}, r: {}", ip, op, operands, result_index);
+
+    let mut jumped = false;
+    match main_op {
+      1 => {
+        instructions[result_index] = operands[0] + operands[1]
+      },
+      2 => {
+        instructions[result_index] = operands[0] * operands[1]
+      },
+      3 => {
+        instructions[result_index] = input
+      },
+      4 => {
+        println!("Output! {}", operands[0]);
+        outputs.push(operands[0]);
+      },
+      5 => {
+        if operands[0] != 0 {
+          ip = operands[1] as usize;
+          jumped = true;
+        }
+      },
+      6 => {
+        if operands[0] == 0 {
+          ip = operands[1] as usize;
+          jumped = true;
+        }
+      },
+      7 => {
+        instructions[result_index] = if operands[0] < operands[1] {
+          1
+        } else {
+          0
+        };
+      },
+      8 => {
+        instructions[result_index] = if operands[0] == operands[1] {
+          1
+        } else {
+          0
+        };
+      },
+      _ => panic!("At the math disco!")
+    };
+
+    if !jumped {
+      ip += jump_table[main_op as usize];
+    }
     op = instructions[ip];
   }
 
@@ -63,7 +80,7 @@ fn isa_interpreter(instructions: &mut Vec<i32>, input: i32) -> i32 {
   outputs[outputs.len() - 1]
 }
 
-pub fn problem1() {
+pub fn problem1() -> i32 {
   let input = include_str!("./data/input-1.txt");
   let mut opcodes = input
     .split(",")
@@ -76,10 +93,23 @@ pub fn problem1() {
 
   let result = isa_interpreter(&mut opcodes, 1);
   println!("result: {}", result);
+  result
 }
 
-pub fn problem2() {
-  println!("tbd");
+pub fn problem2() -> i32 {
+  let input = include_str!("./data/input-1.txt");
+  let mut opcodes = input
+    .split(",")
+    .filter(|v| *v != "\n")
+    .filter(|v| *v != "")
+    .map(|v| {
+      v.parse::<i32>().unwrap()
+    })
+    .collect::<Vec<_>>();
+
+  let result = isa_interpreter(&mut opcodes, 5);
+  println!("result: {}", result);
+  result
 }
 
 #[cfg(test)]
@@ -87,14 +117,51 @@ mod test {
   use super::*;
 
   #[test]
-  fn problem1_example1() {
-    // assert_eq!(patch_and_interpret_problem1("1,9,10,3,2,3,11,0,99,30,40,50"), 3500);
-
-    // assert_eq!(patch_and_interpret_problem1("1,0,0,0,99"), 2);
+  fn problem1_solution() {
+    assert_eq!(problem1(), 9025675);
   }
 
   #[test]
   fn problem2_example1() {
-    assert_eq!(2, 2);
+    let mut instructions = vec![3,9,8,9,10,9,4,9,99,-1,8];
+    let result = isa_interpreter(&mut instructions, 8);
+    assert_eq!(result, 1);
+  }
+
+  #[test]
+  fn problem2_example2() {
+    let mut instructions = vec![3,9,7,9,10,9,4,9,99,-1,8];
+    let result = isa_interpreter(&mut instructions, 4);
+    assert_eq!(result, 1);
+  }
+
+  #[test]
+  fn problem2_example3() {
+    let mut instructions = vec![3,3,1108,-1,8,3,4,3,99];
+    let result = isa_interpreter(&mut instructions, 8);
+    assert_eq!(result, 1);
+  }
+
+  #[test]
+  fn problem2_example4() {
+    let mut instructions = vec![3,3,1107,-1,8,3,4,3,99];
+    let result = isa_interpreter(&mut instructions, 4);
+    assert_eq!(result, 1);
+  }
+
+  #[test]
+  fn problem2_example5() {
+    let mut instructions = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
+    let result = isa_interpreter(&mut instructions, 3);
+    assert_eq!(result, 1);
+  }
+
+  #[test]
+  fn problem2_example6() {
+    let mut instructions = vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+    1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+    999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
+    let result = isa_interpreter(&mut instructions, 8);
+    assert_eq!(result, 1000);
   }
 }
