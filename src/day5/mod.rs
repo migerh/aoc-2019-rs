9ff0a583ed027cdc418f4ebddb0573fd5fa70470
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2021-12-25 23:19:46 +0100

    Migrate Day 5 to cargo aoc

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,85 +1,78 @@
-use super::intcode::{isa_interpreter};
+use super::intcode::isa_interpreter;
 
-fn load_code() -> Vec<i64> {
-  let input = include_str!("./data/input-1.txt");
-  let opcodes = input
-    .split(",")
-    .filter(|v| *v != "\n")
-    .filter(|v| *v != "")
-    .map(|v| {
-      v.parse::<i64>().unwrap()
-    })
-    .collect::<Vec<_>>();
+#[aoc_generator(day5)]
+fn load_code(input: &str) -> Vec<i64> {
+    let opcodes = input
+        .split(",")
+        .filter(|v| *v != "\n")
+        .filter(|v| *v != "")
+        .map(|v| v.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
 
-  opcodes
+    opcodes
 }
 
-pub fn problem1() -> i64 {
-  let mut opcodes = load_code();
+#[aoc(day5, part1)]
+pub fn problem1(opcodes: &Vec<i64>) -> i64 {
+    let mut opcodes = opcodes.clone();
 
-  let result = isa_interpreter(&mut opcodes, 1);
-  println!("result: {}", result);
-  result
+    isa_interpreter(&mut opcodes, 1)
 }
 
-pub fn problem2() -> i64 {
-  let mut opcodes = load_code();
+#[aoc(day5, part2)]
+pub fn problem2(opcodes: &Vec<i64>) -> i64 {
+    let mut opcodes = opcodes.clone();
 
-  let result = isa_interpreter(&mut opcodes, 5);
-  println!("result: {}", result);
-  result
+    isa_interpreter(&mut opcodes, 5)
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn problem1_solution() {
-    assert_eq!(problem1(), 9025675);
-  }
+    #[test]
+    fn problem2_example1() {
+        let mut instructions = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
+        let result = isa_interpreter(&mut instructions, 8);
+        assert_eq!(result, 1);
+    }
 
-  #[test]
-  fn problem2_example1() {
-    let mut instructions = vec![3,9,8,9,10,9,4,9,99,-1,8];
-    let result = isa_interpreter(&mut instructions, 8);
-    assert_eq!(result, 1);
-  }
+    #[test]
+    fn problem2_example2() {
+        let mut instructions = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
+        let result = isa_interpreter(&mut instructions, 4);
+        assert_eq!(result, 1);
+    }
 
-  #[test]
-  fn problem2_example2() {
-    let mut instructions = vec![3,9,7,9,10,9,4,9,99,-1,8];
-    let result = isa_interpreter(&mut instructions, 4);
-    assert_eq!(result, 1);
-  }
+    #[test]
+    fn problem2_example3() {
+        let mut instructions = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
+        let result = isa_interpreter(&mut instructions, 8);
+        assert_eq!(result, 1);
+    }
 
-  #[test]
-  fn problem2_example3() {
-    let mut instructions = vec![3,3,1108,-1,8,3,4,3,99];
-    let result = isa_interpreter(&mut instructions, 8);
-    assert_eq!(result, 1);
-  }
+    #[test]
+    fn problem2_example4() {
+        let mut instructions = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
+        let result = isa_interpreter(&mut instructions, 4);
+        assert_eq!(result, 1);
+    }
 
-  #[test]
-  fn problem2_example4() {
-    let mut instructions = vec![3,3,1107,-1,8,3,4,3,99];
-    let result = isa_interpreter(&mut instructions, 4);
-    assert_eq!(result, 1);
-  }
+    #[test]
+    fn problem2_example5() {
+        let mut instructions = vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9];
+        let result = isa_interpreter(&mut instructions, 3);
+        assert_eq!(result, 1);
+    }
 
-  #[test]
-  fn problem2_example5() {
-    let mut instructions = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
-    let result = isa_interpreter(&mut instructions, 3);
-    assert_eq!(result, 1);
-  }
-
-  #[test]
-  fn problem2_example6() {
-    let mut instructions = vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-    1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-    999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
-    let result = isa_interpreter(&mut instructions, 8);
-    assert_eq!(result, 1000);
-  }
+    #[test]
+    fn problem2_example6() {
+        let mut instructions = vec![
+            3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0,
+            0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4,
+            20, 1105, 1, 46, 98, 99,
+        ];
+        let result = isa_interpreter(&mut instructions, 8);
+        assert_eq!(result, 1000);
+    }
 }
